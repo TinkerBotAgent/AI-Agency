83ecbc1e83180944995329c44297a0eeecb06c12
TinkerBotAgent <kimmistryset@mindfullstudios.org>
2025-10-10 23:24:03 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
+  experimental: {
+    esmExternals: false,
   },
+  images: {
+    unoptimized: true
+  },
+}
+
+module.exports = nextConfig
   eslint: {
     ignoreDuringBuilds: true,
   },
