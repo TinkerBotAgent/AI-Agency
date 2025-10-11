cfaf225b08aa9aca3663920e55c4a7a9d0f5572f
TinkerBotAgent <kimmistryset@mindfullstudios.org>
2025-10-10 23:28:28 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    esmExternals: false,
+    esmExternals: false
   },
   images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
     unoptimized: true
   },
 }
